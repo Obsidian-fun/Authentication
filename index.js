6435d7fa394cbf92aae027bff0bd07bee3810229
goodStuff-trustMeBro <obsidiancrystalmaze@gmail.com>
2024-01-11 22:01:32 -0800

    Adding sign up, log-in verification

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import path from 'path';
 import cors from 'cors';
+import 'dotenv/config';
+import router from './routes/router.js';
 
 const app = express();
 app.listen (3800, ()=>{
@@ -10,21 +9,6 @@ app.listen (3800, ()=>{
 });
 
 app.use(cors('http://localhost:3800'));
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname,'/')));
-
-// Root is the login page
-app.get('/',(req, res) => {
-  res.sendFile(join(__dirname, 'login.html'));
-});
-
-// On hitting register, go to registration page
-app.get('/register', (req,res, err) => {
-  if (err) {
-    console.error(err);
-  }
-  res.sendFile(__dirname + '/register.html');
-});
+app.use('/', router);
 
 
---
+++ routes/router.js
@@ -0,0 +1,126 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import bcrypt from 'bcryptjs'; // Find out right ES6 import import { v4 as uuid } from 'uuid';
+import jwt from 'jsonwebtoken';  // Find out right ES6 import
+import { connection } from '../lib/db.js';
+import { validateRegister } from '../middleware/users.js'; 
+const router = express.Router();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+//router.use(express.static(path.join(__dirname + '/var/www/html/authentication'));
+
+// Root is the login page
+ router.get('/',(req, res) => {
+  res.sendFile(join(__dirname,'../login.html'));
+});
+
+// On hitting register, go to registration page
+router.get('/signup', (req,res, err) => {
+  if (err) {
+    console.error(err);
+  }
+  res.sendFile(join(__dirname,'../register.html'));
+});
+
+
+// Register route,
+ router.post('/signup', validateRegister, (req, res, next) => {
+ if (err) {
+  console.error(err);
+  }
+  connection.query(`SELECT * FROM users WHERE LOWER(username)=LOWER(?);`, 
+    [req.body.username],
+    (err, result) => {
+      if(result && result.length) {
+        return res.status(409).send({
+          message:"The username is already in use :(",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash)=> {
+          if (err) {
+            return res.status(500).send({
+              message: err,
+            });
+          } else {
+        connection.query(`INSERT INTO users (id, username, password, registered) VALUES (?, ?, ?, now());`, [uuid, req.body.username, hash],
+          (err, result) => {
+            if (err) {
+              return res.status(400).send({
+                message:err,
+              });
+            }
+            return res.status(201).send({
+              message: "Registered!",
+            });
+          }
+        );
+       }
+      });
+    }
+    }
+  );
+ });
+      
+ router.post('/login',validateRegister, (req, res , next) => {
+  connection.query(
+    `SELECT * FROM users WHERE username=?;`,
+    [req.body.username],
+    (err, result) => {
+      if(err) {
+        return res.status(400).send({
+          message:err,
+        });
+      }
+      if(!result.length) {
+        return res.status(400).send({
+          message:'Username or password incorrect!',
+        });
+      }
+
+      bcrypt.compare(
+        req.body.password, result[0]['password'],
+        (bErr, bResult)=>{
+          if(bErr){ //passwords do not match
+            return res.status(400).send({
+              message:'Username or password incorrect!',
+            });
+          }
+        if(bResult){ // when passwords do match,
+          const token=jwt.sign(
+          {
+            username:result[0].username,
+            userId:result[0].id,
+          },
+          'SECRET KEY',
+            { expiresIn: '7d'}          
+          );
+          db.query(`UPDATE users SET last_login = now() WHERE id=?;`,
+          [result[0].id,]);
+          return res.status(200).send({
+            message:"Logged in!",
+            token,
+            user:result[0],
+          });
+        }
+          return res.status(400).send({
+            message:"Username or password incorrect!",
+          });
+        }
+        );
+      }
+      );
+      });
+
+  
+// Only allow authenticated users to access the chat application,
+ router.get('/secret-route',(req,res,err) => {
+ if (err) {
+   console.error(err);
+ }
+// res.sendFile(join(__dirname,'../nice_chat_app.html'));
+ });
+
+export default router;
