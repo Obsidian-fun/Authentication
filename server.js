00452fa66a90f0cd94334506d3715d0f81e89415
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-01-25 20:12:17 +0530

    Added registration for users

--- server.js
+++ server.js
@@ -8,15 +8,27 @@ import jwt from 'jsonwebtoken';  // Find out right ES6 import
 import 'dotenv/config';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import mysql from 'mysql';
+import { v4 as uuidv4 } from 'uuid';
 
+// Connecting server to listen on a port,
 let port = process.env.PORT || 3000;
 
 app.use(cors()) // Enable ALL cross origin sharing ( DANGEROUS )
 app.use(bodyParser.json())
 
-
+// setting the default path
 const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(__dirname);
+
+// connecting database to server,
+const connection = mysql.createConnection({
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  database : process.env.DB_NAME,
+  password : process.env.DB_PASSWORD,
+});
+connection.connect();
 
 app.get('/', (req,res)=> {
   res.sendFile(join(__dirname,'login.html'));
@@ -45,7 +57,6 @@ app.get('/signup', (req,res)=> {
 });
 */
 
-
 app.post('/register', (req, res)=>{
   let { username, email, password } = req.body;
   console.log({ username, email, password }); 
@@ -53,17 +64,37 @@ app.post('/register', (req, res)=>{
   const user = { name:username };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
-  res.status(200).send({
+  bcrypt.hash(password, 10, (err, hash)=> {
+    if(err){
+      res.status(500).send({
+        message:err,
+      })
+    } else {
+      connection.query(`INSERT INTO users (id, username, password, registered) VALUES (?, ?, ?, NOW());`,[uuidv4(), username, hash], 
+      (err, result) => {
+        if (err) {
+          return res.status(400).send({
+            message:err,
+          });
+        }
+        return res.status(200).send({
+          message:"Registered",
+        });
+      }
+     ); 
+    }
+  });
+});
+
+/*  res.status(200).send({
     message:"Registered",
     accessToken:accessToken,
   }); 
-
-});
+*/
 
 app.get('/secret-route', (req, res)=> {
   res.sendFile(join(__dirname,'/secret.html'));
 });
-
 
 app.listen(port, ()=> {
   console.log(`Server listening on ${port}`);
