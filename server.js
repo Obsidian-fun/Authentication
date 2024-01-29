b6b64d5d7be02b1f547cdf58f058a05869d16f35
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-01-29 09:56:28 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -42,6 +42,29 @@ app.get('/signup', (req,res)=> {
 
 /*
 // Middleware functions,
+unction validateRegister(req, res, next) {
+  // Username should be greater than 3 characters
+  if(!req.body.username || req.body.username < 3) {
+    return res.status(400).send({
+      message:'Please enter a username with min. 3 characters',
+    });
+  }
+  // Password should be greater than 6 characters
+  if(!req.body.password || req.body.password < 6) {
+    return res.status(400).send({
+      message: 'Please enter a password greater than 6 characters',
+    });
+  }
+  // Passwords must match,
+  if(!req.body.password_repeat || req.body.password_repeat != req.body.password) {
+    return res.status(400).send({
+      message: 'Passwords must match..',
+    });
+  }
+  next();
+}
+
+
 function isLoggedIn(req, res, next) {
   if(!req.headers.authorization) {
     return res.status(401).send({
